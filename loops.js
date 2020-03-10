0e52942cdf9b20ba393ec48a38646f91091da1b6
Nikita Savelev <nsavelev@yahoo.com>
2020-03-10 20:27:34 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array){
   for(let i=0;i<26;i+=1){
-    return `"I am ${i} strange loop${i === 0 ? '' : 's'}."`
+    return `I am ${i} strange loop${i === 0 ? '' : 's'}.`
   }
 }
 function whileLoop(n){
